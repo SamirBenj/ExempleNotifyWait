20713cdf07d4f399fe59110d7b3d502d583e2010
SamirBenj <sciencecalc7@gmail.com>
2022-01-12 10:38:21 +0100

    new class

--- src/Consommateur.java
+++ src/Consommateur.java
@@ -1,3 +1,16 @@
-public class Consommateur {
+public class Consommateur extends Thread{
+    Fonctions f;
+    public Consommateur(Fonctions f) {
+        this.f = f;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for(int i=0; i<4;i++){
+            f.consommer();
+        }
+    }
 
 }
+
