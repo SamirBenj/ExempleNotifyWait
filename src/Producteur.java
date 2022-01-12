4a8b2e1de3713fd2932968d0d08f1feca8b1a189
SamirBenj <sciencecalc7@gmail.com>
2022-01-12 10:36:02 +0100

    new class

--- src/Producteur.java
+++ src/Producteur.java
@@ -1,3 +1,16 @@
-public class Producteur {
+public class Producteur extends Thread {
 
+    Fonctions f;
+    public  Producteur(Fonctions f){
+        this.f =f;
+
+    }
+    public void run(){
+        f.produire("message 1");
+        f.produire("message 2");
+        try {
+            sleep(500);
+        }catch (InterruptedException e){}
+        f.produire("message 3");
+    }
 }
